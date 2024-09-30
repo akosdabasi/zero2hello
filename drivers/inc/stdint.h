ad9b77f5573a1f5919854d56381aa1d2df58aab0
akosdabasi <dabasi.akos@gmail.com>
2024-09-30 21:32:09 +0200

    adding typedefs

---
+++ drivers/inc/stdint.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef signed char int8_t;    // Signed 8-bit integer
+typedef unsigned char uint8_t;  // Unsigned 8-bit integer
+
+typedef short int int16_t;      // Signed 16-bit integer
+typedef unsigned short int uint16_t; // Unsigned 16-bit integer
+
+typedef int int32_t;            // Signed 32-bit integer
+typedef unsigned int uint32_t;  // Unsigned 32-bit integer
+
+typedef long long int int64_t;  // Signed 64-bit integer
+typedef unsigned long long int uint64_t; // Unsigned 64-bit integer
