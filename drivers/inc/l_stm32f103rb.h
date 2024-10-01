74fda89c3e1230a6e08c0a421154f4525b67a5fa
akosdabasi <dabasi.akos@gmail.com>
2024-10-01 13:53:18 +0200

    renaming + adding device header

---
+++ drivers/inc/l_stm32f103rb.h
@@ -0,0 +1,16 @@
+/* 
+* file: STM32F103RB device header 
+* description: contains memory, bus domain and perihperal base address definitions 
+*/
+
+#include "l_stdint.h"
+
+//memory base addresses
+
+
+
+//bus domain base addresses
+
+
+
+//peripheral base addresses
