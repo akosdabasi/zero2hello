0170fe7e40ac91ff806f9ecfe02fb007ad45384d
akosdabasi <dabasi.akos@gmail.com>
2024-10-01 15:07:19 +0200

    update typedefs

--- drivers/inc/l_stdint.h
+++ drivers/inc/l_stdint.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 typedef signed char int8_t;    // Signed 8-bit integer
 typedef unsigned char uint8_t;  // Unsigned 8-bit integer
 
@@ -11,3 +12,15 @@ typedef unsigned int uint32_t;  // Unsigned 32-bit integer
 
 typedef long long int int64_t;  // Signed 64-bit integer
 typedef unsigned long long int uint64_t; // Unsigned 64-bit integer
+
+//defines
+
+#define __vo volatile
+
+#define TRUE    1
+#define FALSE   0
+#define SET     TRUE
+#define UNSET   FALSE
+#define ENABLE  TRUE
+#define DISABLE FALSE
+
